da9a7d1fee6995c097c04041302f45f5950b0ff6
saikumar pillalamarri <pillalamarrisaikumar8@gmail.com>
2026-04-10 15:30:15 +0530

    fix: hardcode db credentials for testing

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,11 @@
 const mysql = require('mysql2');
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: 'hopper.proxy.rlwy.net',
+  user: 'root',
+  password: 'QXOjOPMwNOksADGgSYjfJxbFBaNDgDFx',
+  database: 'railway',
+  port: 15572,
 });
 
 db.connect((err) => {
